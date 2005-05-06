394b292e8bcf9bcb9afb3590358ae1dd8ceb1fbe
l_m <>
2005-05-06 01:56:54 +0000

    Removed the ACDDialog class and resources

--- acdmon/resource.h
+++ acdmon/resource.h
@@ -6,10 +6,6 @@
 #define IDB_BACKGROUND                  130
 #define IDB_FOREGROUND                  131
 #define IDB_STEP                        132
-#define IDD_DIALOG                      133
-#define IDC_SLIDER1                     1000
-#define IDC_BUTTON1                     1001
-#define IDC_BUTTON2                     1002
 
 // Next default values for new objects
 // 
@@ -17,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
