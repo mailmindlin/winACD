11f16cf40ece133b796f71b102f492e3e2cb24d7
mailmindlin <liamttlyrox@gmail.com>
2019-04-16 22:10:56 -0400

    Tabs not spaces

--- acdmon/ACDMon.cpp
+++ acdmon/ACDMon.cpp
@@ -35,15 +35,15 @@ END_MESSAGE_MAP()
 /** CACDMonApp construction. */
 CACDMonApp::CACDMonApp ()
 {
-    // enumerate the ACD controls.
-    EnumHelper helper;
-    CACDHidDevice::EnumDevices (helper);
+	// enumerate the ACD controls.
+	EnumHelper helper;
+	CACDHidDevice::EnumDevices (helper);
 }
 
 /** CACDMonApp destructor */
 CACDMonApp::~CACDMonApp ()
 {
-    for (INT_PTR i = 0; i <  m_DeviceArray.GetCount (); ++i)
+	for (INT_PTR i = 0; i <  m_DeviceArray.GetCount (); ++i)
 	delete m_DeviceArray.ElementAt (i);
 }
 
@@ -56,123 +56,122 @@ CACDMonApp theApp;
 BOOL
 CACDMonApp::InitInstance ()
 {
-    // required on WinXP with the use of ComCtl32.dll version 6 or later.
-    InitCommonControls ();
+	// required on WinXP with the use of ComCtl32.dll version 6 or later.
+	InitCommonControls ();
 
-    // application initialization (we might not need the registry stuff).
-    CWinApp::InitInstance ();
-    SetRegistryKey (_T ("WinACD"));
+	// application initialization (we might not need the registry stuff).
+	CWinApp::InitInstance ();
+	SetRegistryKey (_T ("WinACD"));
 
-    // create the background window.
-    CACDBrightnessWnd* pBackgroundWnd = new CACDBrightnessWnd (
+	// create the background window.
+	CACDBrightnessWnd* pBackgroundWnd = new CACDBrightnessWnd (
 	CACDBrightnessWnd::ACD_OSD_WND_BACKGROUND);
-    pBackgroundWnd->Create (NULL, NULL);
+	pBackgroundWnd->Create (NULL, NULL);
 
-    // create the foreground window.
-    CACDBrightnessWnd* pForegroundWnd = new CACDBrightnessWnd (
+	// create the foreground window.
+	CACDBrightnessWnd* pForegroundWnd = new CACDBrightnessWnd (
 	CACDBrightnessWnd::ACD_OSD_WND_FOREGROUND,
 	pBackgroundWnd
 	);
-    pForegroundWnd->Create (NULL, NULL);
-    pForegroundWnd->SetOpacity (0);
-    pForegroundWnd->ShowWindow (SW_HIDE);
-    pForegroundWnd->UpdateWindow ();
+	pForegroundWnd->Create (NULL, NULL);
+	pForegroundWnd->SetOpacity (0);
+	pForegroundWnd->ShowWindow (SW_HIDE);
+	pForegroundWnd->UpdateWindow ();
 
-    // the foreground window will be the one receiving events.
-    m_pMainWnd = pForegroundWnd;
+	// the foreground window will be the one receiving events.
+	m_pMainWnd = pForegroundWnd;
 
-    // create & start the RegNotify thread.
-    AfxBeginThread (RegNotifyThreadMain, 0);
+	// create & start the RegNotify thread.
+	AfxBeginThread (RegNotifyThreadMain, 0);
 
-    DEV_BROADCAST_DEVICEINTERFACE NotificationFilter;
-    ZeroMemory (&NotificationFilter, sizeof (NotificationFilter));
-    NotificationFilter.dbcc_size = sizeof (DEV_BROADCAST_DEVICEINTERFACE);
-    NotificationFilter.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
-    HidD_GetHidGuid (&NotificationFilter.dbcc_classguid);
-    m_hDevNotify = RegisterDeviceNotification (
+	DEV_BROADCAST_DEVICEINTERFACE NotificationFilter;
+	ZeroMemory (&NotificationFilter, sizeof (NotificationFilter));
+	NotificationFilter.dbcc_size = sizeof (DEV_BROADCAST_DEVICEINTERFACE);
+	NotificationFilter.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
+	HidD_GetHidGuid (&NotificationFilter.dbcc_classguid);
+	m_hDevNotify = RegisterDeviceNotification (
 	pForegroundWnd->m_hWnd,
 	&NotificationFilter,
 	DEVICE_NOTIFY_WINDOW_HANDLE
 	);
 
-    return TRUE;
+	return TRUE;
 }
 
 int
-CACDMonApp::ExitInstance ()
-{
-    UnregisterDeviceNotification (m_hDevNotify);
-    return CWinApp::ExitInstance ();
+CACDMonApp::ExitInstance() {
+	UnregisterDeviceNotification(m_hDevNotify);
+	return CWinApp::ExitInstance();
 }
 
 
 UINT
 CACDMonApp::RegNotifyThreadMain (LPVOID pParam)
 {
-    HKEY hKey;
+	HKEY hKey;
  
-    if (RegCreateKeyEx (
-	    HKEY_CURRENT_USER, "SOFTWARE\\WinACD\\Preferences", 0, 0,
-	    REG_OPTION_NON_VOLATILE, KEY_NOTIFY, 0, &hKey, 0
-	    ) != ERROR_SUCCESS)
+	if (RegCreateKeyEx (
+		HKEY_CURRENT_USER, "SOFTWARE\\WinACD\\Preferences", 0, 0,
+		REG_OPTION_NON_VOLATILE, KEY_NOTIFY, 0, &hKey, 0
+		) != ERROR_SUCCESS)
 	return -1;
 
-    while (TRUE) {
+	while (TRUE) {
 	theApp.m_pMainWnd->SendMessage (ACD_WM_INIT_HOTKEYS, 0, 0);
 
 	if (RegNotifyChangeKeyValue (hKey, FALSE,
 		REG_NOTIFY_CHANGE_LAST_SET, 0, FALSE
 		) != ERROR_SUCCESS)
-	    break;
-    }
+		break;
+	}
 
-    RegCloseKey (hKey);
+	RegCloseKey (hKey);
 
-    return 0;
+	return 0;
 }
 
 CACDMonApp::EnumHelper::ENUMPROC_STATUS
 CACDMonApp::EnumHelper::Callback (CACDHidDevice* pDevice)
 {
-    ENUMPROC_STATUS status = CACDHidDevice::EnumHelper::Callback (pDevice);
+	ENUMPROC_STATUS status = CACDHidDevice::EnumHelper::Callback (pDevice);
 
-    if (status != ENUMPROC_STATUS_SUCCESS)
+	if (status != ENUMPROC_STATUS_SUCCESS)
 	return status;
 
-    theApp.m_DeviceArray.Add (pDevice);
-    return ENUMPROC_STATUS_SUCCESS;
+	theApp.m_DeviceArray.Add (pDevice);
+	return ENUMPROC_STATUS_SUCCESS;
 }
 
 BOOL
 CACDMonApp::GetBrightness (PUCHAR pbBrightness)
 {
-    if (m_DeviceArray.GetCount () == 0)
+	if (m_DeviceArray.GetCount () == 0)
 	return FALSE;
 
-    BOOL bRet = m_DeviceArray.ElementAt (0)->GetBrightness (pbBrightness);
-    if (bRet)
+	BOOL bRet = m_DeviceArray.ElementAt (0)->GetBrightness (pbBrightness);
+	if (bRet)
 	*pbBrightness /= 16;
 
-    return bRet;
+	return bRet;
 }
 
 BOOL
 CACDMonApp::SetBrightness (UCHAR nBrightness)
 {
-    nBrightness = min (nBrightness, 15);
-    nBrightness = nBrightness * 16 + nBrightness;
+	nBrightness = min (nBrightness, 15);
+	nBrightness = nBrightness * 16 + nBrightness;
 
-    BOOL bRet = TRUE;
-    for (INT_PTR i = 0; i <  m_DeviceArray.GetCount (); ++i)
+	BOOL bRet = TRUE;
+	for (INT_PTR i = 0; i <  m_DeviceArray.GetCount (); ++i)
 	bRet &= m_DeviceArray.ElementAt (i)->SetBrightness (nBrightness);
 
-    return bRet;
+	return bRet;
 }
 
 BOOL
 CACDMonApp::OnDeviceChange (UINT nEventType, LPTSTR lpcDeviceName)
 {
-    if (nEventType == DBT_DEVICEREMOVECOMPLETE
+	if (nEventType == DBT_DEVICEREMOVECOMPLETE
 	|| nEventType == DBT_DEVICEARRIVAL) {
 	// at some point we might check if we actually have an open
 	// handle for the disconnected device. for now, we just purge
@@ -180,7 +179,7 @@ CACDMonApp::OnDeviceChange (UINT nEventType, LPTSTR lpcDeviceName)
 
 	INT_PTR iCount = m_DeviceArray.GetCount ();
 	for (INT_PTR i = 0; i <  iCount; ++i)
-	    delete m_DeviceArray.ElementAt (i);
+		delete m_DeviceArray.ElementAt (i);
 
 	m_DeviceArray.RemoveAll ();
 
@@ -188,7 +187,7 @@ CACDMonApp::OnDeviceChange (UINT nEventType, LPTSTR lpcDeviceName)
 	CACDHidDevice::EnumDevices (helper);
 
 	return iCount != m_DeviceArray.GetCount ();
-    }
+	}
 
-    return FALSE;
+	return FALSE;
 }
